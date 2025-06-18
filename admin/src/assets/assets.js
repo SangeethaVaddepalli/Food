0b24f5594b712f8626bc4d6d93842df6b1a562af
Sangeetha Vaddepalli <140165326+SangeethaVaddepalli@users.noreply.github.com>
2025-06-19 01:09:01 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://food-del-backend-yqqz.onrender.com'
+// export const url = 'https://food-del-backend-yqqz.onrender.com'
+export const url = 'https://food-backend-i5nu.onrender.com'
 export const currency = '₹'
 export const assets ={
     logo,
